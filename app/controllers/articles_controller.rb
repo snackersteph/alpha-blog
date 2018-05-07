15dea7bdb55e34b200873e897b93f744cac2cbab
swongwrites <swongwrites@gmail.com>
2018-05-07 15:44:28 -0400

    Add article and category association

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,15 +11,26 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
+  def edit
+  end
+  
   def create
     @article = Article.new(article_params)
     @article.user = current_user
-   
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
     else
       render 'new'
+    end
+  end
+  
+  def update
+    if @article.update(article_params)
+      flash[:success] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
     end
   end
   
@@ -32,33 +43,20 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
   
-  
-  def edit
-  end
-  
-  def update
-    if @article.update(article_params)
-      flash[:success] = "article was successfully updated"
-      redirect_to article_path(@article)
-    else
-      render 'edit'
+  private
+    def set_article
+      @article = Article.find(params[:id])  
     end
-  end
   
-private
-  def set_article
-    @article = Article.find(params[:id])
-  end
-  
-  def article_params
-    params.require(:article).permit(:title, :description)
-  end
-  
-  def require_same_user
-    if current_user != @article.user and !current_user.admin?
-      flash[:danger] = "You can only edit or delete your own articles"
-      redirect_to root_path
+    def article_params
+      params.require(:article).permit(:title, :description, category_ids: [])
     end
-  end
+  
+    def require_same_user
+      if current_user != @article.user and !current_user.admin?
+        flash[:danger] = "You can only edit or delete your own articles"
+        redirect_to root_path
+      end
+    end
   
 end
